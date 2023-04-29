d7b563d13741f9bddf9d2c6ff1d82c8e8c9ea640
DakaD <3106338+Dakad@users.noreply.github.com>
2023-04-29 21:48:51 +0200

    Refactor WebdriverSessionHelper to also use firefox browser

--- spec/support/webdriver_session_helper.cr
+++ spec/support/webdriver_session_helper.cr
@@ -1,7 +1,10 @@
 require "selenium"
 
 class WebdriverSessionHelper
+  ALLOWED_TYPES = %w(chrome firefox)
+
   @@session : Selenium::Session | Nil
+  @@driver  : Selenium::Driver | Nil
 
   def self.base_url
     "https://bwilczek.github.io/watir_pump_tutorial"
@@ -9,22 +12,71 @@ class WebdriverSessionHelper
 
   def self.session
     @@session ||= begin
-      chrome_options = Selenium::Chrome::Capabilities::ChromeOptions.new
-      chrome_options.args = ["no-sandbox", "disable-gpu"]
-      chrome_options.args << "headless" if ENV.has_key?("CI")
-
-      capabilities = Selenium::Chrome::Capabilities.new
-      capabilities.chrome_options = chrome_options
-
-      chromedriver_path = `which chromedriver`.chomp
-
-      driver = Selenium::Driver.for(:chrome, service: Selenium::Service.chrome(driver_path: chromedriver_path))
-      session = driver.create_session(capabilities)
+      session = driver.not_nil!.create_session(browser_options)
 
       Spec.after_suite do
-        driver.stop
+        session.window_manager.close_window
+        driver.not_nil!.stop
       end
+
       session
     end
+  end
+
+  def self.driver
+    @@driver ||= case browser_type
+                 when "chrome"
+                   driver_path = `which chrome`.chomp
+                   driver_service = Selenium::Service.chrome(driver_path: driver_path)
+                   Selenium::Driver.for(:chrome, service: driver_service)
+                 when "firefox"
+                   driver_path = `which geckodriver`.chomp
+                   driver_service = Selenium::Service.firefox(driver_path:  driver_path)
+                   Selenium::Driver.for(:firefox, service: driver_service)
+                 end
+  end
+
+  def self.browser_type
+    type = ENV.fetch("RUN_ON", "firefox")
+    raise "Invalid value for browser_type: #{type} - Allowed values #{ALLOWED_TYPES}" unless ALLOWED_TYPES.includes? type
+    type
+  end
+
+  def self.browser_options
+    capabilities = case browser_type
+                   when "firefox"
+                     cap = Selenium::Firefox::Capabilities.new
+                     cap.firefox_options = firefox_browser_options
+                     cap
+                   when "chrome"
+                     cap = Selenium::Chrome::Capabilities.new
+                     cap.chrome_options = chrome_browser_options
+                     cap
+                   end
+    capabilities
+  end
+
+  def self.firefox_browser_options
+    options = Selenium::Firefox::Capabilities::FirefoxOptions.new
+    options.args = ["--safe-mode", "--devtools"]
+    options.args << "--headless" if ENV.has_key?("CI")
+
+    if ENV.has_key?("RUN_WITH_PROFILE")
+      if profile_name = ENV["BROWSER_PROFILE_NAME"]?
+        options.args << "-P #{profile_name}"
+      elsif profile_path = ENV["BROWSER_PROFILE_PATH"]?
+        options.args << "--profile #{profile_path}"
+      else
+        raise "Missing BROWSER_PROFILE_NAME or BROWSER_PROFILE_PATH env config"
+      end
+    end
+    options
+  end
+
+  def self.chrome_browser_options
+    options = Selenium::Chrome::Capabilities::ChromeOptions.new
+    options.args = ["no-sandbox", "disable-gpu"]
+    options.args << "headless" if ENV.has_key?("CI")
+    options
   end
 end
